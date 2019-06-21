6f1d3b2ac926fcae93e2cf00d83094476c900f2e
booth08-mgr2 <booth08-mgr2@MGR2-BTH108W>
2019-06-21 12:08:16 +0200

    Commit inicial

---
+++ Migrations/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Migrations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Album {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public MusicStyle Style { get; set; }
+    }
+
+    enum MusicStyle{
+        Grunge,
+        Soul,
+        Blues,
+        Hiphop,
+        Metal
+    }
+
+    class MusicDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer(@"Data source=(localdb)\MSSQLLocalDB; Initial Catalog=Movies;Integrated Security=true");
+        }
+        public DbSet<Album> Albums {get; set;}
+    }
+}
