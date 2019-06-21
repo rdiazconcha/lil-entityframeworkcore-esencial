93759edf4d99c7bd7aab578fda29cf4111c3278c
booth08-mgr2 <booth08-mgr2@MGR2-BTH108W>
2019-06-21 11:22:57 +0200

    Inserción

--- Movies/Program.cs
+++ Movies/Program.cs
@@ -7,7 +7,30 @@ namespace Movies
     {
         static void Main(string[] args)
         {
-            
+            System.Console.WriteLine("Escriba el nombre de la película:");
+            var name = Console.ReadLine();
+            System.Console.WriteLine("Escriba el año de estreno:");
+            var year = Console.ReadLine();
+
+            var newMovie = new Movie();
+            newMovie.Name = name;
+            newMovie.Year = int.Parse(year);
+
+            var db = new MoviesDbContext();
+            db.Movies.Add(newMovie);
+
+            var result = db.SaveChanges();
+
+            if (result == 1){
+                System.Console.WriteLine("La película ha sido guardada exitosamente");
+            }
+            else{
+                System.Console.WriteLine("Error");
+            }
+
+            System.Console.WriteLine("Presione cualquier tecla para salir...");
+            Console.ReadLine();
+
         }
     }
 
