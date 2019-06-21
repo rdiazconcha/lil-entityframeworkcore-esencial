15f0a58dab109c1996575f89e7c7c3bc452f8f0e
booth08-mgr2 <booth08-mgr2@MGR2-BTH108W>
2019-06-21 10:23:26 +0200

    Modelo y DbContext

--- Movies/Program.cs
+++ Movies/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace Movies
 {
@@ -6,7 +7,25 @@ namespace Movies
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
+    }
+
+    public class Movie {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+    }
+
+    public class MoviesDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer(@"Data source=(localdb)\MSSQLLocalDB; Initial Catalog=Movies;Integrated Security=true");
+        }
+        protected MoviesDbContext()
+        {
+        }
+
+        public DbSet<Movie> Movies {get; set;}
     }
 }
