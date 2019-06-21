8665eda381022a36533745ed7a33f9e6af677efb
booth08-mgr2 <booth08-mgr2@MGR2-BTH108W>
2019-06-21 06:31:29 +0200

    Cadena de conexión

--- WebApiCursos/Context/CoursesDbContext.cs
+++ WebApiCursos/Context/CoursesDbContext.cs
@@ -11,7 +11,7 @@ namespace WebApiCursos.Context
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("");
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Courses;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
 
         public DbSet<Course> Courses { get; set; }
