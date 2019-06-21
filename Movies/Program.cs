c1e589223037ee583521e0b3dc055892ea597768
booth08-mgr2 <booth08-mgr2@MGR2-BTH108W>
2019-06-21 10:25:05 +0200

    Clases privadas

--- Movies/Program.cs
+++ Movies/Program.cs
@@ -11,13 +11,13 @@ namespace Movies
         }
     }
 
-    public class Movie {
+    class Movie {
         public int Id { get; set; }
         public string Name { get; set; }
         public int Year { get; set; }
     }
 
-    public class MoviesDbContext : DbContext
+    class MoviesDbContext : DbContext
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
             optionsBuilder.UseSqlServer(@"Data source=(localdb)\MSSQLLocalDB; Initial Catalog=Movies;Integrated Security=true");
