85018d4fa270da8cbea0da203908fc6503f9a7a5
booth08-mgr2 <booth08-mgr2@MGR2-BTH108W>
2019-06-21 06:23:00 +0200

    CoursesDbContext

---
+++ WebApiCursos/Context/CoursesDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiCursos.Models;
+
+namespace WebApiCursos.Context
+{
+    public class CoursesDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
